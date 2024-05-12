6185642b06badd951fa7dd4e72656fc741340c5e
nnisarggada <gadannisarg@gmail.com>
2024-05-12 20:51:58 +0530

    fixes

--- i3.sh
+++ i3.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
-sudo pacman -S git
+if ! command -v git &> /dev/null
+then
+    sudo pacman -S --noconfirm git && curl -sL https://synced.nnisarg.in/shared/archinstall/i3.sh | bash
+    exit 1
+fi
 
-mkdir $HOME/repos
-cd $HOME/repos
+mkdir -p ~/repos || {
+    echo "Failed to create directory. Aborting."
+    exit 1
+}
 
-git clone --depth=1 https://github.com/nnisarggada/i3-dots
-cd i3-dots && ./install.sh
+cd ~/repos || {
+    echo "Failed to change directory. Aborting."
+    exit 1
+}
+
+git clone --depth=1 https://github.com/nnisarggada/i3-dots || {
+    echo "Failed to clone repository. Aborting."
+    exit 1
+}
+
+cd i3-dots || {
+    echo "Failed to change directory. Aborting."
+    exit 1
+}
+
+./install.sh || {
+    echo "Failed to run install script. Aborting."
+    exit 1
+}
+
+echo "Done!"
